601373a0a89e1911bd0801f0862c123d39e0a501
ABDERRAHMAN AHAITAF <AHAITAF.ABDERRAHMAN@ofppt-edu.ma>
2022-06-03 15:44:04 +0100

    Q5

--- app/src/main/java/com/example/cc2abderrahmanahaitaftdi201/MyDatabase.java
+++ app/src/main/java/com/example/cc2abderrahmanahaitaftdi201/MyDatabase.java
@@ -1,8 +1,12 @@
 package com.example.cc2abderrahmanahaitaftdi201;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class MyDatabase extends SQLiteOpenHelper {
     public static String db_name = "societes.db";
@@ -29,6 +33,57 @@ public class MyDatabase extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(sql);
         onCreate(sqLiteDatabase);
     }
+
+    public long AddSociete(SQLiteDatabase db, Societe e){
+        ContentValues cv = new ContentValues();
+        cv.put(col1,e.getId());
+        cv.put(col2,e.getNom());
+        cv.put(col3,e.getSecteur());
+        cv.put(col4,e.getNombreEmploye());
+        return db.insert(table_name,null,cv);
+    }
+
+    public static long deleteSociete(SQLiteDatabase db,String number){
+
+        return db.delete(table_name, "ID="  +col1,null);
+
+    }
+
+
+    public long UpdateSociete(SQLiteDatabase db, Societe e){
+        ContentValues cv = new ContentValues();
+        cv.put(col1,e.getId());
+        cv.put(col2,e.getNom());
+        cv.put(col3,e.getSecteur());
+        cv.put(col4,e.getNombreEmploye());
+        return db.update(table_name,cv,"ID="+ col1,null);
+    }
+
+
+    public static ArrayList<Societe> getAllSociete (SQLiteDatabase db) {
+        Societe s = null;
+        Cursor c = db.rawQuery(String.format("select * from %s where ID = %d", table_name, s.getId()), null);
+        ArrayList<Societe> lstSo = new ArrayList<>();
+        while (c.moveToNext()) {
+            lstSo.add(new Societe(c.getInt(0), c.getString(1), c.getString(2), c.getInt(3)));
+        }
+        return lstSo;
+    }
+
+    public static Societe getOneSociete (SQLiteDatabase db){
+        Societe s = null;
+        Cursor c = db.rawQuery(String.format("select * from %s where ID = %d",table_name, s.getId()),null);
+        if(c.moveToNext()){
+            s = new Societe(c.getInt(0),c.getString(1),c.getString(2),c.getInt(3));
+        }
+        return  s;
+    }
+
+
+
+
+
+
 
 
 
